64464328bd7a561a1b60cab650e3cad060df6148
zyud <zyud@seas.upenn.edu>
2018-04-17 00:11:38 -0400

    Add JSON data parsing and display. Add F to C display conversion.

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-// Load temperature from Arduino
+// Temperature display and JSON data parsing
+var isCelsius = true;
+var currentTempC = 0;
+var highestTempC = 0;
+var lowestTempC = 0;
+var averageTempC = 0;
+var graphTempVals = [];
+var graphTimeVals = [];
+
+document.getElementById('toggle-temp').addEventListener('click', function() {
+  isCelsius = isCelsius ? false : true;
+  updateTempDisplay();
+});
+
 loadTemp();
 
 function loadTemp() {
   console.log('calling load temp');
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'data.txt', true);
+  xhr.open('GET', 'data.json', true);
 
   xhr.onload = function() {
     if (this.status == 200) {
-      document.getElementById('current-temp').innerHTML = this.responseText;
+      var temp = JSON.parse(this.responseText);
+      graphTempVals.push(temp.current);
+      graphTimeVals.push(Date.now());
+
+      currentTempC = temp.current;
+      highestTempC = temp.highest;
+      lowestTempC = temp.lowest;
+      averageTempC = temp.average;
+
+      updateTempDisplay();
     } else {
       document.getElementById('current-temp').innerHTML = 'No Data';
+      document.getElementById('lowest-temp').innerHTML = 'No Data';
+      document.getElementById('highest-temp').innerHTML = 'No Data';
+      document.getElementById('average-temp').innerHTML = 'No Data';
     }
   };
 
   xhr.send()
   setTimeout(loadTemp, 5000);
+}
+
+function updateTempDisplay() {
+  document.getElementById('current-temp').innerHTML = isCelsius ?
+      currentTempC + "&deg;C" : currentTempC * 9 / 5 + 32 + "&deg;F";
+  document.getElementById('lowest-temp').innerHTML = isCelsius ?
+      lowestTempC + "&deg;C" : lowestTempC * 9 / 5 + 32 + "&deg;F";
+  document.getElementById('highest-temp').innerHTML = isCelsius ?
+      highestTempC + "&deg;C" : highestTempC * 9 / 5 + 32 + "&deg;F";
+  document.getElementById('average-temp').innerHTML = isCelsius ?
+      averageTempC + "&deg;C" : averageTempC * 9 / 5 + 32 + "&deg;F";
 }
 
 // Elapsed time display
